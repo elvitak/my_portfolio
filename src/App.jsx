4faaa7fc71ab6b1f826fd4c6fadce214752b99d8
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-11-16 14:21:44 +0100

    add structure and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Hello from "./Hello";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <Hello />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
