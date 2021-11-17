0b7ef518f39da957906ab6a4bfed072dee87b54a
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-11-17 12:53:47 +0100

    prepare stage for displaying projects from json file

---
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -0,0 +1,33 @@
+describe("User can see list of projects", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+    cy.get("#projects-tab").click();
+  });
+  it("dispalys the first project", () => {
+    cy.get("#project-1").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".header").should("contain", "My First Website");
+      cy.get(".description").should("contain", "This was my first project.");
+    });
+  });
+  it("displays the second projects", () => {
+    cy.get("#project-2").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".header").should("contain", "FizzBuzz");
+      cy.get(".description").should(
+        "contain",
+        "Making ruby backend application is fun."
+      );
+    });
+  });
+  it("displays the third projects", () => {
+    cy.get("#project-3").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".header").should("contain", "MobileUX");
+      cy.get(".description").should(
+        "contain",
+        "I like to design for the mobile platform."
+      );
+    });
+  });
+});
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
+import axios from "axios";
 
-const Projects = () => {
-  return (
-    <Container>
-      <h1 id="projects-header">My Projects</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-    </Container>
-  );
-};
+class Projects extends Component {
+  state = {
+    projects: [],
+    componentDidMount() {
+      axios.get("./data/projects.json").then((response) => {
+        this.setState({ projects: response.data });
+      });
+    },
+  };
+
+  render() {
+    const { projects } = this.state;
+
+    let projectsList = projects.map((project) => {
+      return (
+        <div id={`project-${project.id}`} key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      );
+    });
+    return (
+      <Container>
+        <h1 id="projects-header">My Projects</h1>
+        {projectsList}
+      </Container>
+    );
+  }
+}
 
 export default Projects;
