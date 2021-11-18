f55f7954b8a497bc412e2c0434e1c82ccb16504f
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-11-18 13:55:53 +0100

    convert Projects component from class component to functional component

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,35 +1,31 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Grid } from "semantic-ui-react";
 import axios from "axios";
 import ProjectCard from "./ProjectCard";
 
-class Projects extends Component {
-  state = {
-    projects: [],
-  };
+const Projects = () => {
+  const [projects, setProjects] = useState([]);
 
-  render() {
-    const { projects } = this.state;
-
-    let projectsList = projects.map((project) => {
-      return (
-        <div id={`project-${project.id}`} key={project.id}>
-          <ProjectCard project={project} />
-        </div>
-      );
-    });
-    return (
-      <Container>
-        <h1 id="projects-header">My Projects</h1>
-        <Grid>{projectsList}</Grid>
-      </Container>
-    );
-  }
-  componentDidMount() {
+  useEffect(() => {
     axios.get("./data/projects.json").then((response) => {
-      this.setState({ projects: response.data });
+      setProjects(response.data);
     });
-  }
-}
+  }, []);
+
+  let projectsList = projects.map((project) => {
+    return (
+      <div id={`project-${project.id}`} key={project.id}>
+        <ProjectCard project={project} />
+      </div>
+    );
+  });
+
+  return (
+    <Container>
+      <h1 id="projects-header">My Projects</h1>
+      <Grid>{projectsList}</Grid>
+    </Container>
+  );
+};
 
 export default Projects;
